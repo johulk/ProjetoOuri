d42d38a59b49490350551f315db451d69fe3a2be
johulk <igor1brag@hotmail.com>
2021-04-18 15:54:15 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -5,7 +5,7 @@ var config = {
     type: Phaser.CANVAS,
     width: DEFAULT_WIDTH,
     height: DEFAULT_HEIGHT,
-    backgroundColor: 0x000000,
+    backgroundColor: 0xffffff,
     pixelArt: false,
     antialias: true,
     scene: [Load, Menu, Pvp, Pvc, Dificuldade],
